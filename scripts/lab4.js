fbc6933875d47b3c728faa93366e898947a531cb
ominjoo <o.minjoo14@gmail.com>
2025-04-29 17:21:44 -0700

    retestinf fixed code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
+    if (add && typeof num1 === 'number' && typeof num2 === 'number') {
         let result = 0;
 
         result = num1 + num2;
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -26,7 +26,7 @@ function sumValues(num1, num2, add) {
  */
 function discountPrices(prices, discount) {
     let discounted = []
-    const length = prices.length;
+    const length = prices.length - 1;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
         discountedPrice += prices[i] * (1 - discount);
